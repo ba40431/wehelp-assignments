6639253d6eb4a38a22f408940e5f47fed207115d
Abby <94294207+ba40431@users.noreply.github.com>
2022-01-20 23:11:01 +0800

    Add files via upload

--- week_2/week_2.py
+++ week_2/week_2.py
@@ -43,20 +43,20 @@ print(result)
 
 
 
-def maxProduct(nums):
+def maxProduct(nums):# N代表時間複雜度 4N
     # 請用你的程式補完這個函式的區塊
-    max_num = max(nums)#m1是最大元素
-    list_copy = nums.copy()#複製一個列表，同時不破壞原來的列表
-    list_copy.remove(max_num)#把列表里最大的元素刪除
-    secondmax_num = max(list_copy)#再次取列表里最大的元素，這時取到的就是列表里第二大的元素
+    max_num = max(nums)#m1是最大元素 (N)
+    list_copy = nums.copy()#複製一個列表，同時不破壞原來的列表 (1)
+    list_copy.remove(max_num)#把列表里最大的元素刪除 (1)
+    secondmax_num = max(list_copy)#再次取列表里最大的元素，這時取到的就是列表里第二大的元素 (N)
     #m1是第二大的值,m2是最大值
-    min_num = min(nums)
-    list_copy = nums.copy()
-    list_copy.remove(min_num)
-    secondmmin_num = min(list_copy)
+    min_num = min(nums) #N
+    list_copy = nums.copy()# 1
+    list_copy.remove(min_num) # 1
+    secondmmin_num = min(list_copy) #N
     #m4是第二小的值,m3是最小值
-    max_sum=max_num*secondmax_num
-    min_sum=min_num*secondmmin_num
+    max_sum=max_num*secondmax_num #1
+    min_sum=min_num*secondmmin_num #1
     if max_sum>=min_sum:
         return max_sum
     else: return min_sum
@@ -83,4 +83,30 @@ def twoSum(nums, target):
 
 result=twoSum([2, 11, 7, 15], 9)
 print(result) # show [0, 2] because nums[0]+nums[2] is 9
+
+def maxZeros(nums):
+    # 請用你的程式補完這個函式的區塊
+    sum=0
+    max0=0
+    for i in nums:
+        if i==0:
+            sum=sum+1
+        else:
+            if max0<sum:
+                max0=sum
+                sum=0
+
+    if sum>max0:
+        return sum
+    else:    
+        return max0
+        
+result=maxZeros([0, 1, 0, 0]) # 得到 2
+print(result)
+result=maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]) # 得到 4
+print(result)
+result=maxZeros([1, 1, 1, 1, 1]) # 得到 0
+print(result)
+result=maxZeros([0, 0, 0, 1, 1]) # 得到 3
+print(result)
 
---
+++ week_2/week_2_0120.py
@@ -0,0 +1,27 @@
+def maxZeros(nums):
+    # 請用你的程式補完這個函式的區塊
+    sum=0
+    max0=0
+    for i in nums:
+        if i==0:
+            sum=sum+1
+        else:
+            if max0<sum:
+                max0=sum
+                sum=0
+
+    if sum>max0:
+        return sum
+    else:    
+        return max0
+        
+result=maxZeros([0, 1, 0, 0]) # 得到 2
+print(result)
+result=maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]) # 得到 4
+print(result)
+result=maxZeros([1, 1, 1, 1, 1]) # 得到 0
+print(result)
+result=maxZeros([0, 0, 0, 1, 1]) # 得到 3
+print(result)
+result=maxZeros([0, 0, 0, 1, 1, 0 ,0, 0, 0,1]) # 得到 4
+print(result)
