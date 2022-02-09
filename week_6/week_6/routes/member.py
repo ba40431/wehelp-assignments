a769f32757125b7b9620aeb6aecc6cfbd46ca2d0
Abby <94294207+ba40431@users.noreply.github.com>
2022-02-09 15:03:21 +0800

    Delete week_6/week_6 directory

--- week_6/week_6/routes/member.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,Flask,request,redirect,render_template,session
-
-member_info=Blueprint("member",__name__,static_folder="static",template_folder="templates")
-
-@member_info.route("/member/")
-def member():
-    if session["username"]: #有值
-         return render_template("member.html",name=session["name"])
-    else:return redirect("/")
