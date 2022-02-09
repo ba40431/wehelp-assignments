f300e63976cb5a149d6493789618b034db12b2e5
Abby <94294207+ba40431@users.noreply.github.com>
2022-02-09 15:03:00 +0800

    Add files via upload

---
+++ week_6/week_6/routes/signin.py
@@ -0,0 +1,37 @@
+from flask import Blueprint,Flask,request,redirect,render_template,session
+import mysql.connector
+
+signin_info=Blueprint("signin",__name__,static_folder="static",template_folder="templates")
+
+@signin_info.route("/signin",methods=["POST"])
+def signin():
+    username=request.form["username"]
+    password=request.form["password"]
+
+    connection=mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="12345678",
+        database="website"
+    )
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM `member` WHERE `username`=%s ;",[username])
+    member=cursor.fetchone()
+
+    if member==None:
+        if username==''or password=='':
+            return redirect("/error/?message=請輸入帳號、密碼")
+        else:return redirect("/error/?message=帳號或密碼輸入錯誤")
+    if member: #有值
+        if username==member[2] and  password==member[3]:
+            session["username"]= username
+            session["name"]=member[1]
+            return redirect("/member/")
+        elif username==''or password=='':
+            return redirect("/error/?message=請輸入帳號、密碼")
+        else:
+            return redirect("/error/?message=帳號或密碼輸入錯誤")
+
+    cursor.close()
+    connection.close()
