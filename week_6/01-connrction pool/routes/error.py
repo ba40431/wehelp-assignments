bbb4db4717b93522d2d6c85e3a1e6019eadd528a
Abby <94294207+ba40431@users.noreply.github.com>
2022-02-10 11:34:11 +0800

    Delete week_6/01-connrction pool directory

--- week_6/01-connrction pool/routes/error.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint,Flask,request,redirect,render_template,session
-
-error_message=Blueprint("error",__name__,static_folder="static",template_folder="templates")
-
-@error_message.route("/error/")
-def error():
-    error_message=request.args.get("message","自訂的錯誤訊息")  
-    return render_template("error.html",message=error_message)
