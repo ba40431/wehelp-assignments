c2225ad5348a0ac57e0413d904bf1bc49b7faa8b
Abby <94294207+ba40431@users.noreply.github.com>
2022-02-11 13:56:52 +0800

    Add files via upload

--- week_6/routes/signin.py
+++ week_6/routes/signin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,Flask,request,redirect,render_template,session
+import os
+from dotenv import load_dotenv
 import mysql.connector
-from connection import pool
 
 signin_info=Blueprint("signin",__name__,static_folder="static",template_folder="templates")
 
@@ -9,7 +10,15 @@ def signin():
     username=request.form["username"]
     password=request.form["password"]
 
-    connection=pool.connection()
+    load_dotenv(".env")
+    connection=mysql.connector.connect(
+        host=os.getenv("host"),
+        port=os.getenv("port"),
+        user=os.getenv("user"),
+        password=os.getenv("password"),
+        database=os.getenv("database")
+    )
+    
     cursor=connection.cursor()
     cursor.execute("SELECT * FROM `member` WHERE `username`=%s ;",[username])
     member=cursor.fetchone()
@@ -18,9 +27,8 @@ def signin():
         if username==''or password=='':
             return redirect("/error/?message=請輸入帳號、密碼")
         else:return redirect("/error/?message=帳號或密碼輸入錯誤")
-
-    if member:
-        if username==member[2] and password==member[3]:
+    if member: #有值
+        if username==member[2] and  password==member[3]:
             session["username"]= username
             session["name"]=member[1]
             return redirect("/member/")
--- week_6/routes/signup.py
+++ week_6/routes/signup.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,Flask,request,redirect,render_template,session,flash
+import os
+from dotenv import load_dotenv
 import mysql.connector
-from connection import pool
 
 signup_info=Blueprint("signup",__name__,static_folder="static",template_folder="templates")
 
@@ -10,7 +11,15 @@ def signup():
     register_username=request.form["register_username"]
     register_password=request.form["register_password"]
 
-    connection=pool.connection()
+    load_dotenv(".env")
+    connection=mysql.connector.connect(
+        host=os.getenv("host"),
+        port=os.getenv("port"),
+        user=os.getenv("user"),
+        password=os.getenv("password"),
+        database=os.getenv("database")
+    )
+    
     cursor=connection.cursor()
     cursor.execute("SELECT * FROM `member` WHERE `username`=%s ;",[register_username])
     member=cursor.fetchone()
@@ -20,7 +29,7 @@ def signup():
             return redirect("/error/?message=請輸入姓名、帳號和密碼")
         else:
             cursor.execute("INSERT INTO `member`(`name`,`username`,`password`) VALUES (%s,%s,%s);",
-            (name, register_username,register_password)) #%s 利用佔位符傳遞參數
+            (name, register_username, register_password)) #%s 利用佔位符傳遞參數，防範SQL injection
             cursor.close()
             connection.commit()
             connection.close()
